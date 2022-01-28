6a208bcc683be026f9c8347f961e8cb19c120a91
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-01-28 23:35:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './components/ui/Header'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../../public/breakingbad_logo.png'
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={logo} alt=''></img>
+    </header>
+  )
+}
+
+export default Header
